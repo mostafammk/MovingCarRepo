30d9005311f2f2dda5c6d3edc29da94923a15350
mostafammkhalil <mostafa.m.k@ieee.org>
2019-09-26 13:38:04 +0200

    Updating the sevensegment driver after testing it on the developement kit

--- SevenSegment.c
+++ SevenSegment.c
@@ -1,8 +1,32 @@
-/*
- * SevenSegment.c
+/******************************************************************************
  *
- *  Created on: Sep 25, 2019
- *      Author: Mostafa-Khalil
- */
+ * Module: Seven Segment
+ *
+ * File Name: seven_segment.c
+ *
+ * Description: Source file for Seven Segment
+ *
+ * Author: Mahmoud Rezk Mahmoud
+ *
+ *******************************************************************************/
 
+#include "SevenSegment.h"
 
+uint8 SEVEN_SEGMENT_displayDigit(uint8 digit){
+
+	uint8 func_status = OK;
+	DIO_write(SEVEN_SEGMENT_ENABLE_PORT,SEVEN_SEGMENT_ENABLE_PIN,HIGH);
+	if((digit>=0) && (digit<=9))
+	{
+		DIO_write(SEVEN_SEGMENT_PORT,SEVEN_SEGMENT_PIN0,(1u&(digit>>0)));
+		DIO_write(SEVEN_SEGMENT_PORT,SEVEN_SEGMENT_PIN1,(1u&(digit>>1)));
+		DIO_write(SEVEN_SEGMENT_PORT,SEVEN_SEGMENT_PIN2,(1u&(digit>>2)));
+		DIO_write(SEVEN_SEGMENT_PORT,SEVEN_SEGMENT_PIN3,(1u&(digit>>3)));
+	}
+	else
+	{
+		func_status = NOK;
+	}
+
+	return func_status;
+}
--- SevenSegment.h
+++ SevenSegment.h
@@ -1,13 +1,32 @@
-/*
- * SevenSegment.h
+/******************************************************************************
  *
- *  Created on: Sep 25, 2019
- *      Author: Mostafa-Khalil
+ * Module: Seven Segment
+ *
+ * File Name: seven_segment.c
+ *
+ * Description: Header file for Seven Segment
+ *
+ * Author: Mahmoud Rezk
+ *
+ *******************************************************************************/
+
+#ifndef SEVEN_SEGMENT_H_
+#define SEVEN_SEGMENT_H_
+
+#include "DIO.h"
+
+
+#define SEVEN_SEGMENT_PORT PORT_B
+#define SEVEN_SEGMENT_PIN0 PIN0
+#define SEVEN_SEGMENT_PIN1 PIN1
+#define SEVEN_SEGMENT_PIN2 PIN2
+#define SEVEN_SEGMENT_PIN3 PIN3
+
+#define SEVEN_SEGMENT_ENABLE_PORT PORT_D
+#define SEVEN_SEGMENT_ENABLE_PIN PIN2
+/*
+ * Function Prototypes for Seven Segment
  */
+uint8 SEVEN_SEGMENT_displayDigit(uint8 digit);
 
-#ifndef SEVENSEGMENT_H_
-#define SEVENSEGMENT_H_
-
-
-
-#endif /* SEVENSEGMENT_H_ */
+#endif /* SEVEN_SEGMENT_H_ */
