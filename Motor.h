be4e48d7d9040142b9658cb596325fb7bbf1e3eb
Mahmoud Rezk <mahmoud.rezk.843@gmail.com>
2019-09-26 23:26:02 +0200

    add Motor driver files with all test cases (Static code and configuration)

--- Motor.h
+++ Motor.h
@@ -7,7 +7,15 @@
 
 #ifndef MOTOR_H_
 #define MOTOR_H_
-
+#include "DIO.h"
+#include "MOTOR_confg.h"
+#define MOTOR_CTRL_REG PORTD
+#define MOTOR_ENA_PIN PIN5
+#define MOTOR_DIR_PIN1 PIN6
+#define MOTOR_DIR_PIN2 PIN7
+uint8 Motor_Forward(void);
+uint8 Motor_Reverse(void);
+uint8 Motor_Stop(void);
 
 
 #endif /* MOTOR_H_ */
