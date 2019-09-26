483774f299c2da58843f406cddd01f657a2c6f47
Mahmoud Rezk <mahmoud.rezk.843@gmail.com>
2019-09-26 21:34:50 +0200

    Adding Motor Driver files header file and source file

---
+++ Motor.c
@@ -0,0 +1,8 @@
+/*
+ * Motor.c
+ *
+ *  Created on: Sep 26, 2019
+ *      Author: Mahmoud Rezk Mahmoud , Mustafa Khalil
+ */
+
+
---
+++ Motor.h
@@ -0,0 +1,13 @@
+/*
+ * Motor.h
+ *
+ *  Created on: Sep 26, 2019
+ *      Author: Mahmoud Rezk Mahmoud , Mustafa Khalil
+ */
+
+#ifndef MOTOR_H_
+#define MOTOR_H_
+
+
+
+#endif /* MOTOR_H_ */
